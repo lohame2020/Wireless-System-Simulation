d61b13b39f93e441b5497751f108d47c2bce457b
Marcus1911 <marcus.sandri@ufscar.br>
2014-11-01 14:26:23 -0200

    s[n]+awgn(t) filter by FFT

--- telecom.m
+++ telecom.m
@@ -1,45 +1,31 @@
 clear all; close all;
+fs = 10000;
+t = 0:1/fs:1.5;
+x1 = sawtooth(2*pi*50*t);
+x2 = square(2*pi*50*t)
 
-#
-# In this program we simulate the effects of an additive noise into a signal source s(t). 
-#
-#  s(t)-----(sum)-------r(t)
-#             |
-#             |
-#            n(t)
+noise = awgn(t, 0.1);
 
-
-# 
-# Transmitter 
-#
-
-x = 1:100;
-voice = sin(x);
-
-# Convert to digital 
-
-digital = sign(voice);
-#area(x,digital);
+coding = fft(x2);
 
 
-#
-# Channel
-#
-
-noise = awgn(voice,-20);
-
-# additive channel
-
-raw_receiver = noise + digital;
+result = coding + noise;
 
 
-subplot(2, 1, 1)
-area(x, digital);
-subplot(2, 1, 2)
-area(x, raw_receiver);
+rcoding = ifft(result)
 
-# Sent/Received
+subplot(211)
+plot(t,x2)
+axis([0 0.2 -1.2 1.2]);
+xlabel('Time (sec)');
+ylabel('Amplitude');
+title('Square Periodic Wave');
 
-pre_ber = digital - raw_receiver;
-bits_received = find(pre_ber < -1); 
-BER = numel(bits_received)/numel(digital)  
+
+subplot(212)
+plot(t,rcoding)
+axis([0 0.2 -1.2 1.2]);
+xlabel('Time (sec)');
+ylabel('Amplitude');
+title('Square Periodic Wave on Additive Channel');
+

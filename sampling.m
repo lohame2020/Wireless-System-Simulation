36a09fcabd1e8db1860e0e43cefdaade4a01166d
Marcus1911 <marcus.sandri@ufscar.br>
2014-11-01 14:27:31 -0200

    Fourier series transforming an waveform.

--- sampling.m
+++ sampling.m
@@ -1,32 +1,43 @@
 clear all; close all;
-fs = 10000;
-t = 0:1/fs:1.5;
-x1 = sawtooth(2*pi*50*t);
-x2 = square(2*pi*50*t)
 
-noise = awgn(t, 0.1);
+T = 2; 
+w0 = 2*pi/T;
+T1 = T/4;
+harmax = 80;
+Npontos = 10000;
+tmin = -2;
+tmax = 2;
+t = linspace(tmin, tmax, Npontos);
+a0 = 2*T1/T;
+serie(1,:) = a0*ones(1, Npontos);
 
-coding = fft(x2);
+for k = 1:harmax,
+	ak = sin(k*w0*T1)/(k*pi);
+        amk = sin(-k*w0*T1)/(-k*pi);
+        serie(k+1,:) = serie(k,:)+ak*exp(j*k*w0*t)+amk*exp(j*(-k)*w0*t);
+end
 
+temposquad = [-2 -1.5 -1.5 -0.5 -0.5 0.5 0.5 1.5 1.5 2];
+quad = [1 1 0 0 1 1 0 0 1 1];
+subplot(321); plot(t, serie(2,:));grid;title('DC + 1^a harmonica');
+hold on; plot(temposquad, quad, 'k');
 
-result = coding + noise;
+subplot(322); plot(t, serie(4,:));grid;
+title('DC + 1^a harmonica + 3^a harmonica');
+hold on; plot(temposquad, quad, 'k');
 
+subplot(323); plot(t, serie(8,:));grid;
+title('DC + 1^a harmonica + 7^a harmonica');
+hold on; plot(temposquad, quad, 'k');
 
-rcoding = ifft(result)
+subplot(324); plot(t, serie(20,:));grid;
+title('DC + 1^a harmonica + 19^a harmonica');
+hold on; plot(temposquad, quad, 'k');
 
-subplot(211)
-plot(t,x2)
-axis([0 0.2 -1.2 1.2]);
-xlabel('Time (sec)');
-ylabel('Amplitude');
-title('Square Periodic Wave');
+subplot(323); plot(t, serie(80,:));grid;
+title('DC + 1^a harmonica + 3^a harmonica');
+hold on; plot(temposquad, quad, 'k');
+xlabel('t');
 
-
-subplot(212)
-plot(t,rcoding)
-axis([0 0.2 -1.2 1.2]);
-xlabel('Time (sec)');
-ylabel('Amplitude');
-title('Square Periodic Wave on Additive Channel');
 
 
